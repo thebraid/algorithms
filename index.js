f2877c496e0785cc3fbc6cb15564478b19358b5e
klester <thebraid1@gmail.com>
2018-04-07 16:10:11 +0300

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const data = [
+    {name: 'test1', value: 53},
+    {name: 'test2', value: 2},
+];
+
+const reduceArray = (array = data) => {
+    const resultObj = {};
+
+    for (let i = 0; i < array.length; i++) {
+        resultObj[array[i].name] = array[i].value;
+    }
+
+    console.log(resultObj);
+};
+
