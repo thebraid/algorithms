2d1431cb575f453baec68a18ac3e30e2186d532e
klester <thebraid1@gmail.com>
2018-04-07 18:34:00 +0300

    add choiceSort

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function createStairs(n) {
 const initArr = [5,4,3,2,1];
 
 // пузырьковая сортировка
-    function bubbleSort(arr = initArr) {
+function bubbleSort(arr = initArr) {
     let length = arr.length;
 
 
@@ -69,5 +69,47 @@ const initArr = [5,4,3,2,1];
 
 // bSort([4,5, 3,2,1]);
 
-// сортировка вставкой
 // сортировка выбором
+// сортировка вставкой
+
+const choiceArr = [7,4,7,9,4,2,7,4,65,24,54,23,12,64,32,36,83,12,15];
+
+function choiceSort(arr = choiceArr) {
+    let length = arr.length - 1;
+
+    function getMaxIndex(arr) {
+        let max = 0;
+        let maxInd = 0;
+
+        for (let i = 0; i <= length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                maxInd = i;
+            }
+        }
+
+        return maxInd;
+    }
+
+    while(length !== -1) {
+        let maxIndex = getMaxIndex(arr);
+        let last = arr[length];
+
+        if (maxIndex === length -1) {
+            length = length - 1;
+            continue;
+        }
+
+        arr[length] = arr[maxIndex];
+        arr[maxIndex] = last;
+
+        length = length - 1;
+    }
+
+    console.log(arr);
+}
+
+choiceSort();
+
+// расставить 8 ферзей
+// отрезок
